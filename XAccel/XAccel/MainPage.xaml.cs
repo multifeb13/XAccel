66e7e2799a0a49c4f345653b373c458d8b3de580
hirotaka <musicfair@gmail.com>
2017-06-25 11:13:08 +0900

    加速度の取得、画面表示

--- XAccel/XAccel/MainPage.xaml.cs
+++ XAccel/XAccel/MainPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DeviceMotion.Plugin;
+using DeviceMotion.Plugin.Abstractions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,9 +11,52 @@ namespace XAccel
 {
     public partial class MainPage : ContentPage
     {
+        IDeviceMotion motion = CrossDeviceMotion.Current;
+
+        double accelX;
+        double accelY;
+        double accelZ;
+        int changeCount = 0;
+        const int averageNum = 5;
+ 
         public MainPage()
         {
             InitializeComponent( );
+        }
+
+        private void StartClicked(object sender, EventArgs e)
+        {
+            motion.Start( MotionSensorType.Accelerometer, MotionSensorDelay.Default );
+            if( motion.IsActive(MotionSensorType.Accelerometer))
+            {
+                motion.SensorValueChanged += ( object s, SensorValueChangedEventArgs a ) =>
+                {
+                    Device.BeginInvokeOnMainThread( () =>
+                    {
+                        accelX += ( (MotionVector)a.Value ).X;
+                        accelY += ( (MotionVector)a.Value ).Y;
+                        accelZ += ( (MotionVector)a.Value ).Z;
+                        changeCount++;
+
+                        if( changeCount == averageNum )
+                        {
+                            xLabel.Text = "x = " + (accelX / averageNum).ToString( "0.000" );
+                            yLabel.Text = "y = " + (accelY / averageNum).ToString( "0.000" );
+                            zLabel.Text = "z = " + (accelZ / averageNum).ToString( "0.000" );
+
+                            accelX = 0;
+                            accelY = 0;
+                            accelZ = 0;
+                            changeCount = 0;
+                        }
+                    } );
+                };
+            }
+        }
+
+        private void StopClicked( object sender, EventArgs e )
+        {
+            motion.Stop( MotionSensorType.Accelerometer );
         }
     }
 }
